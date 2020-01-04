718c2cd90da417ce7d026023d671fa723572f3e0
Eric Chong <echong@iisl.com>
2020-01-03 21:21:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
 <h1>Hello World
-  <p>From server: <?php
-    echo $_SERVER['SERVER_ADDR']
-    echo "<p>The time is " . date("h:i:sa")
+  <p>From server:
+    <?php
+    echo $_SERVER['SERVER_ADDR'];
+    echo "<p>The time is " . date("h:i:sa");
     ?>
 </html>
