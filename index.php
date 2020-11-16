ac74bfac33fb8f6383525d52c9b4dbbe432d94ec
Eric C Chong <echong@redhat.com>
2020-11-16 17:43:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,8 @@
     echo $_SERVER['SERVER_ADDR'];
     echo "<p>The time is " . date("h:i:sa");
     ?>
+  <p>Environment variable "my-message":
+    <?php
+    echo $_ENV['my-message']
+      ?>
 </html>
